8a1ca153d9b35aa5fd060ba707e1faae2ce55517
Edward Cox Brecht <edbrecht@gmail.com>
2019-12-10 20:41:20 -0800

    added erase(), size(), and pop_front/back() functions

--- CSM10B Project 4 - Linked List/LinkedList.h
+++ CSM10B Project 4 - Linked List/LinkedList.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <memory>
 
 template <typename T>
 struct node
@@ -16,6 +15,8 @@ public:
 	{
 		head = nullptr;
 		tail = nullptr;
+		current = nullptr;
+		previous = nullptr;
 		length = 0;
 	}
 
@@ -23,33 +24,168 @@ public:
 	
 	void push_back(const T &element)
 	{
-		node<T> *temp = new node<T>;
-		temp->data = element;
-		temp->next = nullptr;
+		node<T> *newNode = new node<T>;
+		newNode->data = element;
+		newNode->next = nullptr;
 		++length;
 		if (head == nullptr)
 		{
-			head = temp;
-			tail = temp;
-			temp = nullptr;
+			head = newNode;
+			tail = newNode;
+			newNode = nullptr;
 		}
 		else
 		{
-			tail->next = temp;
-			tail = temp;
+			tail->next = newNode;
+			tail = newNode;
 		}
+		
 	}
 
 	void push_front(const T &element)
 	{
-		node<T> *temp = new node<T>;
-		temp->data = element;
-		temp->next = head;
-		head = temp;
+		node<T> *newNode = new node<T>;
+		newNode->data = element;
+		newNode->next = head;
+		head = newNode;
 		++length;
+		
 	}
 
+bool insert(const T &element, unsigned int position)
+{
+	// check for valid position
+	if ((position < 1) || (position > (length + 1)))
+	{
+		return false;
+	}
+	else
+	{
+		// if inserting at beginning or end use  proper functions
+		if (position == 1)
+		{
+			push_front(element);
+			return true;
+		}
+		if (position == length + 1)
+		{
+			push_back(element);
+			return true;
+		}
+
+
+		node<T> *newNode = new node<T>;
+		current = head;
+		// cycle to desired position
+		for (size_t i = 1; i < position; ++i)
+		{
+			previous = current;
+			current = current->next;
+		}
+		// insert at desired position
+		newNode->data = element;
+		previous->next = newNode;
+		newNode->next = current;
+		++length;
+		return true;
+	}
+	return false;
+}
+
+bool erase(unsigned int position)
+{
+	// check for valid position
+	if ((position < 1) || (position > (length + 1)))
+	{
+		return false;
+	}
+	else
+	{
+		current = head;
+		// cycle to desired position
+		for (size_t i = 1; i < position; ++i)
+		{
+			previous = current;
+			current = current->next;
+		}
+		// establish new link and delete node
+		previous->next = current->next;
+		delete current;
+		--length;
+		return true;
+	}
+	return false;
+}
+
+bool erase(const T &element)
+{
+	current = head;
+	for (size_t i = 1; i <= length; ++i)
+	{
+		if (current->data == element)
+		{
+			// delete from front
+			if (i == 1)
+			{
+				head = current->next;
+				delete current;
+				--length;
+				return true;
+			}
+			// delete from back
+			else if (i == length)
+			{
+				tail = previous;
+				previous->next = nullptr;
+				delete current;
+				--length;
+				return true;
+			}
+			// delete from other position
+			else
+			{
+				previous->next = current->next;
+				delete current;
+				--length;
+				return true;
+			}
+		}
+		previous = current;
+		current = current->next;
+	}
+	return false;
+}
+
+unsigned int size() const
+{
+	return length;
+}
+
+void pop_back()
+{
+	current = head;
+	// cycle to end
+	for (size_t i = 1; i < length; ++i)
+	{
+		previous = current;
+		current = current->next;
+	}
+	// delete node
+	tail = previous;
+	previous->next = nullptr;
+	delete current;
+	--length;
+}
+
+void pop_front()
+{
+	current = head;
+	head = head->next;
+	delete current;
+	--length;
+}
+
 private:
-	node<T> *head, *tail;
-	int length;
+	node<T> *head, *tail, *current, *previous;
+	unsigned int length;
 };
--- CSM10B Project 4 - Linked List/Source.cpp
+++ CSM10B Project 4 - Linked List/Source.cpp
@@ -6,6 +6,10 @@ int main()
 	LinkedList<std::string> *stringList = new LinkedList<std::string>;
 	stringList->push_back("Back");
 	stringList->push_front("Front");
-
+	stringList->insert("Middle-1",2);
+	stringList->insert("Middle-2",3);
+	stringList->erase(2);
+	stringList->erase("Back");
+	stringList->pop_front();
 	system("pause");
 }
