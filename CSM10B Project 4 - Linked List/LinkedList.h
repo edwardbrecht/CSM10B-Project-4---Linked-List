03ea870250fa61458d346bb0bba1a96511921867
Edward Cox Brecht <edbrecht@gmail.com>
2019-12-10 21:20:22 -0800

    added remaining functions for assignment and comments

--- CSM10B Project 4 - Linked List/LinkedList.h
+++ CSM10B Project 4 - Linked List/LinkedList.h
@@ -1,3 +1,6 @@
+// Edward Brecht
+// 12/10/2019
+
 #pragma once
 
 template <typename T>
@@ -11,6 +14,9 @@ template <typename T>
 class LinkedList
 {
 public:
+	/// <summary>
+	/// constructor for linked list
+	/// </summary>
 	LinkedList()
 	{
 		head = nullptr;
@@ -20,8 +26,25 @@ public:
 		length = 0;
 	}
 
-	~LinkedList();
+	/// <summary>
+	/// destructor for linked list
+	/// </summary>
+	~LinkedList()
+	{
+		current = head;
+		while (current->next != nullptr)
+		{
+			previous = current;
+			current = current->next;
+			delete previous;
+		}
+		delete current;
+	}
 	
+	/// <summary>
+	/// inserts a node at end of list
+	/// </summary>
+	/// <param name="element">element</param>
 	void push_back(const T &element)
 	{
 		node<T> *newNode = new node<T>;
@@ -42,6 +65,10 @@ public:
 		
 	}
 
+	/// <summary>
+	/// inserts a node at beginning of list
+	/// </summary>
+	/// <param name="element">element</param>
 	void push_front(const T &element)
 	{
 		node<T> *newNode = new node<T>;
@@ -52,138 +79,213 @@ public:
 		
 	}
 
-bool insert(const T &element, unsigned int position)
-{
-	// check for valid position
-	if ((position < 1) || (position > (length + 1)))
+	/// <summary>
+	/// inserts a node at desired position
+	/// </summary>
+	/// <param name="element">element</param>
+	/// <param name="position">position</param>
+	/// <returns>bool</returns>
+	bool insert(const T &element, unsigned int position)
 	{
+		// check for valid position
+		if ((position < 1) || (position > (length + 1)))
+		{
+			return false;
+		}
+		else
+		{
+			// if inserting at beginning or end use  proper functions
+			if (position == 1)
+			{
+				push_front(element);
+				return true;
+			}
+			if (position == length + 1)
+			{
+				push_back(element);
+				return true;
+			}
+			node<T> *newNode = new node<T>;
+			current = head;
+			// cycle to desired position
+			for (size_t i = 1; i < position; ++i)
+			{
+				previous = current;
+				current = current->next;
+			}
+			// insert at desired position
+			newNode->data = element;
+			previous->next = newNode;
+			newNode->next = current;
+			++length;
+			return true;
+		}
 		return false;
 	}
-	else
+
+	/// <summary>
+	/// erases node at desired position
+	/// </summary>
+	/// <param name="position">position</param>
+	/// <returns>bool</returns>
+	bool erase(unsigned int position)
 	{
-		// if inserting at beginning or end use  proper functions
-		if (position == 1)
+		// check for valid position
+		if ((position < 1) || (position > (length + 1)))
 		{
-			push_front(element);
+			return false;
+		}
+		else
+		{
+			current = head;
+			// cycle to desired position
+			for (size_t i = 1; i < position; ++i)
+			{
+				previous = current;
+				current = current->next;
+			}
+			// establish new link and delete node
+			previous->next = current->next;
+			delete current;
+			--length;
 			return true;
 		}
-		if (position == length + 1)
-		{
-			push_back(element);
-			return true;
-		}
+		return false;
+	}
 
-
-		node<T> *newNode = new node<T>;
+	/// <summary>
+	/// erases first node that matches input data
+	/// </summary>
+	/// <param name="element">element</param>
+	/// <returns>bool</returns>
+	bool erase(const T &element)
+	{
 		current = head;
-		// cycle to desired position
-		for (size_t i = 1; i < position; ++i)
+		for (size_t i = 1; i <= length; ++i)
+		{
+			if (current->data == element)
+			{
+				// delete from front
+				if (i == 1)
+				{
+					head = current->next;
+					delete current;
+					--length;
+					return true;
+				}
+				// delete from back
+				else if (i == length)
+				{
+					tail = previous;
+					previous->next = nullptr;
+					delete current;
+					--length;
+					return true;
+				}
+				// delete from other position
+				else
+				{
+					previous->next = current->next;
+					delete current;
+					--length;
+					return true;
+				}
+			}
+			previous = current;
+			current = current->next;
+		}
+		return false;
+	}
+
+	/// <summary>
+	/// returns size of list
+	/// </summary>
+	/// <returns>length</returns>
+	unsigned int size() const
+	{
+		return length;
+	}
+
+	/// <summary>
+	/// deletes node in last position
+	/// </summary>
+	void pop_back()
+	{
+		current = head;
+		// cycle to end
+		for (size_t i = 1; i < length; ++i)
 		{
 			previous = current;
 			current = current->next;
 		}
-		// insert at desired position
-		newNode->data = element;
-		previous->next = newNode;
-		newNode->next = current;
-		++length;
-		return true;
-	}
-	return false;
-}
-
-bool erase(unsigned int position)
-{
-	// check for valid position
-	if ((position < 1) || (position > (length + 1)))
-	{
-		return false;
-	}
-	else
-	{
-		current = head;
-		// cycle to desired position
-		for (size_t i = 1; i < position; ++i)
+		// delete node
+		if (length == 1)
 		{
-			previous = current;
-			current = current->next;
+			head = nullptr;
+			tail = nullptr;
 		}
-		// establish new link and delete node
-		previous->next = current->next;
+		else
+		{
+			tail = previous;
+			previous->next = nullptr;
+		}
 		delete current;
 		--length;
-		return true;
 	}
-	return false;
-}
 
-bool erase(const T &element)
-{
-	current = head;
-	for (size_t i = 1; i <= length; ++i)
+	/// <summary>
+	/// deletes node in first position
+	/// </summary>
+	void pop_front()
 	{
-		if (current->data == element)
+		current = head;
+		if (length > 1)
 		{
-			// delete from front
-			if (i == 1)
-			{
-				head = current->next;
-				delete current;
-				--length;
-				return true;
-			}
-			// delete from back
-			else if (i == length)
-			{
-				tail = previous;
-				previous->next = nullptr;
-				delete current;
-				--length;
-				return true;
-			}
-			// delete from other position
-			else
-			{
-				previous->next = current->next;
-				delete current;
-				--length;
-				return true;
-			}
+			head = head->next;
 		}
-		previous = current;
-		current = current->next;
+		else
+		{
+			head = nullptr;
+			tail = nullptr;
+		}
+		delete current;
+		--length;
 	}
-	return false;
-}
 
-unsigned int size() const
-{
-	return length;
-}
-
-void pop_back()
-{
-	current = head;
-	// cycle to end
-	for (size_t i = 1; i < length; ++i)
+	/// <summary>
+	/// returns data from desired position.
+	/// no bounds checking, and probable memory leak
+	/// </summary>
+	/// <param name="position">position</param>
+	/// <returns>temp->data</returns>
+	T& get_element(unsigned int position) const
 	{
-		previous = current;
-		current = current->next;
+		node<T> *temp = new node<T>;
+		temp = head;
+		for (size_t i = 1; i < position; ++i)
+		{
+			temp = temp->next;
+		}
+		return temp->data;
 	}
-	// delete node
-	tail = previous;
-	previous->next = nullptr;
-	delete current;
-	--length;
-}
 
-void pop_front()
-{
-	current = head;
-	head = head->next;
-	delete current;
-	--length;
-}
+	/// <summary>
+	/// returns data from first position
+	/// </summary>
+	/// <returns>head->data</returns>
+	T& get_front() const
+	{
+		return head->data;
+	}
+
+	/// <summary>
+	/// returns data from last position
+	/// </summary>
+	/// <returns>tail->data</returns>
+	T& get_back() const
+	{
+		return tail->data;
+	}
 
 private:
 	node<T> *head, *tail, *current, *previous;
--- CSM10B Project 4 - Linked List/Source.cpp
+++ CSM10B Project 4 - Linked List/Source.cpp
@@ -1,15 +1,27 @@
+// Edward Brecht
+// 12/10/2019
+
 #include "LinkedList.h"
 #include <string>
+#include <iostream>
 
 int main()
 {
 	LinkedList<std::string> *stringList = new LinkedList<std::string>;
 	stringList->push_back("Back");
 	stringList->push_front("Front");
-	stringList->insert("Middle-1",2);
-	stringList->insert("Middle-2",3);
-	stringList->erase(2);
-	stringList->erase("Back");
+	stringList->insert("Middle-1", 2);
+	stringList->insert("Middle-2", 3);
+	
+	std::cout << stringList->get_front() << std::endl;
+	std::cout << stringList->get_element(2) << std::endl;
+	std::cout << stringList->get_back() << std::endl;
+
+	stringList->erase(3);
 	stringList->pop_front();
+	stringList->pop_back();
+
+	delete stringList;
+
 	system("pause");
 }
